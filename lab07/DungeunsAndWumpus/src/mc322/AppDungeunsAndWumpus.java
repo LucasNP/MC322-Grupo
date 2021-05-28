9e85829cc394afa7cda21e0428f245f5870da9c1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 19:43:04 -0300

    tirei o artigo

--- lab07/DungeunsAndWumpus/src/mc322/AppDungeunsAndWumpus.java
+++ lab07/DungeunsAndWumpus/src/mc322/AppDungeunsAndWumpus.java
@@ -3,7 +3,7 @@ package mc322;
 public class AppDungeunsAndWumpus {
 
 	public static void main(String[] args) {
-		System.out.println("meu pamonha");
+		System.out.println("pamonha");
 		new Game();
 
 	}
